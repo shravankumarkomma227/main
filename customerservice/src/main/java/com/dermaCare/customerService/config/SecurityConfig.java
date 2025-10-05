1fc1ae79b97efc175f60223785fed6b667bcf9bc
shravan <shravankumaryadavkomma@gmail.com>
2025-10-05 18:13:14 +0530

    safe code

--- customerservice/src/main/java/com/dermaCare/customerService/config/SecurityConfig.java
+++ customerservice/src/main/java/com/dermaCare/customerService/config/SecurityConfig.java
@@ -63,27 +63,11 @@ public class SecurityConfig {
 	    }	
 	    
 	    
-	    @Bean
-		public ClientRegistrationRepository clientRegistrationRepository() {
-			return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
-		}
+//	    @Bean
+//		public ClientRegistrationRepository clientRegistrationRepository() {
+//			return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+//		}
 
-		private ClientRegistration googleClientRegistration() {
-			return ClientRegistration.withRegistrationId("google")
-				.clientId("847747434266-ghg1mghtfe6q54bu41iabsaesi0ordkv.apps.googleusercontent.com")
-				.clientSecret("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")
-				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-				.redirectUri("http://localhost:8083/api/login/oauth2/code/google")
-				.scope("openid", "profile", "email", "address", "phone")
-				.authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
-				.tokenUri("https://www.googleapis.com/oauth2/v4/token")
-				.userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
-				.userNameAttributeName(IdTokenClaimNames.SUB)
-				.jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
-				.issuerUri("https://accounts.google.com")
-				.clientName("Google")
-				.build();
-		}
+		
 				
 }
